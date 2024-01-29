a624d154256bd1b522d1bc027610f1ac01b711f4
이승지 <esg0107@pusan.ac.kr>
2024-01-29 16:49:20 +0900

    [S/W 문제해결 기본] 1일차 - View

---
+++ SWEA/1206_View/view.py
@@ -0,0 +1,16 @@
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+for test_case in range(1, 11):
+    T = int(input())
+    buildings = list(map(int, input().split()))
+
+    count = 0
+    for number in range(2, len(buildings) - 2):
+        left_max = max(buildings[number - 2], buildings[number - 1])
+        right_max = max(buildings[number + 2], buildings[number + 1])
+
+        if (buildings[number] - left_max >= 0) and (buildings[number] - right_max >= 0):
+            count += min((buildings[number] - left_max), (buildings[number] - right_max))
+
+    print(f'#{test_case} {count}')
