95d3915e6e01983b6d06c38aba9452a53560bc30
이승지 <esg0107@pusan.ac.kr>
2024-04-03 07:16:43 +0900

    그래프2 추가학습

--- SWEA/그래프2/최소 비용 신장 트리(MST)/kruskal.py
+++ SWEA/그래프2/최소 비용 신장 트리(MST)/kruskal.py
@@ -1,3 +1,5 @@
+# 정점을 고르는 거임
+# 주어진 간선들은 이미 선택된 게 아니라 정점에 따라 선택하거나 선택 안 할 수 있음
 # 전체!! 그래프를 보고, 가중치로 정렬한 다음, 가중치가 가장 작은 간선부터 뽑자
 
 V, E = map(int, input().split())
@@ -9,19 +11,22 @@ edge.sort(key=lambda x:x[2]) # 가중치 작은 거! 최소 비용!
 parents = [i for i in range(V)]  # 대표 원소 배열
 
 def find_set(x):
+
     if parents[x] == x:
         return x
+    # 집합 찾으면서 갱신하여 대표 원소를 빠르게 찾도록 함 -> 경로 압축
+    # 직전 부모(?)나 연결 정보는 사라짐
     parents[x] = find_set(parents[x])
     return parents[x]
 
-def union(x, y):
-    x = find_set(x)
-    y = find_set(y)
+def union(a, b):
+    x = find_set(a)
+    y = find_set(b)
 
     if x==y:
         return
 
-    # 더 작은 루트노드에 합친다(최소 간선 방문도 중요하니께?)
+    # 더 작은 루트노드에 합친다(최소 간선 방문도 중요하니께 -> 가중치에 영향)
     if x < y:
         parents[y] = x
     else:
